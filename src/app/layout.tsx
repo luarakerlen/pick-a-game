5d7c3ada64b82b707ee8dd1098b534b8ad2cf0a5
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-07-02 14:24:10 -0300

    chore: remove unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={inter.className} style={{
+        backgroundColor: "#f5f5f5",
+      }}>{children}</body>
     </html>
   );
 }
